bdab670cabb91d8e5cc1f170bcabda2cd6a0d13d
Mike Hoskins <michoski@cisco.com>
2017-10-26 23:15:33 -0400

    js stalker

---
+++ jscriptdemo.js
@@ -0,0 +1,10 @@
+
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+//alert("Nice to meet you, " + first + " " + last);
+console.log("Nice to meet you, " + first + " " + last);
+
+//alert("Looks like you are " + age + " years old!");
+console.log("Looks like you are " + age + " years old!");
