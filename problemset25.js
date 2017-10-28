20c8e6d16de40f7a8cf4f1f439e48b47a77b9d96
Mike Hoskins <michoski@cisco.com>
2017-10-28 18:07:46 -0400

    add max function

--- problemset25.js
+++ problemset25.js
@@ -1,4 +1,4 @@
-// pass in array
+// pass in array to reverse
 function printReverse(a) {
     for (i=a.length; i >= 0; i--) {
         console.log(a[i]);
@@ -7,8 +7,7 @@ function printReverse(a) {
 printReverse([1,2,3,4]);
 printReverse(["a","b","c"]);
 
-// pass in array
-// true if all items are the same
+// pass in array; true if all items are the same
 function isUniform(a) {
     var r = true;
     for (i=0; i <= a.length-2; i++) {
@@ -23,6 +22,7 @@ console.log(isUniform([2,1,1,1]));
 console.log(isUniform(["a", "b", "p"]));
 console.log(isUniform(["b", "b", "b"]));
 
+// pass in array; sum all items
 function sumArray(a) {
     var t = 0;
     a.forEach(function(item){
@@ -33,3 +33,17 @@ function sumArray(a) {
 sumArray([1,2,3]);
 sumArray([10,3,10,4]);
 sumArray([-5,100]);
+
+// pass in array; print largest item
+function max(a) {
+    var max = 0;
+    a.forEach(function(item){
+        if (item > max) {
+            max = item;
+        }
+    });
+    console.log(max);
+}
+max([1,2,3]);
+max([10,3,10,4]);
+max([-5,100]);
