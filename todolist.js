3455a5ba6ca6bb06934eace9a2cb89b2583634c9
Mike Hoskins <michoski@cisco.com>
2017-10-28 17:38:00 -0400

    javascript todolist

---
+++ todolist.js
@@ -0,0 +1,32 @@
+var todos = [];
+
+var answer;
+while (answer != "quit") {
+    answer = prompt("What would you like to do?");
+    if (answer == "new") {
+        item = prompt("Enter TODO");
+        newTodo(item);
+    } else if (answer == "list") {
+        listTodos();
+    } else if (answer == "delete") {
+        item = prompt("Enter TODO");
+        deleteTodo(item-1);
+    }
+}
+
+function newTodo(item) {
+    todos.push(item);
+}
+
+function deleteTodo(item) {
+    todos.splice(item, 1);
+}
+
+function listTodos() {
+    for (i=0; i<todos.length; i++) {
+        console.log(i+1 + ". " + todos[i]);
+    }
+}
+
+// quit message
+console.log("Goodbye!");
