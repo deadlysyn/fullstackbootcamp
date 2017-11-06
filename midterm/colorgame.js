a7cc487f541ab3a39b4ed37707ff5c8974c32bd5
Mike Hoskins <michoski@cisco.com>
2017-11-05 20:19:32 -0500

    generate board with random colors

--- midterm/colorgame.js
+++ midterm/colorgame.js
@@ -1,50 +1,53 @@
-// select all the elements we need to manipulate...
-var buttonOne = document.getElementById("one");
-var buttonTwo = document.getElementById("two");
-var buttonReset = document.getElementById("reset");
-var oneH1 = document.getElementById("oneh1");
-var twoH1 = document.getElementById("twoh1");
-var total = document.getElementById("total");
-var input = document.getElementById("score");
-var playTo = Number(total.textContent);
 
-input.addEventListener("change", function() {
-    playTo = Number(input.value);
-    total.textContent = input.value;
-    reset();
-});
+// 3 easy, 6 hard
+var boardSize = 6;
+// list of colors for game pieces
+var colors = [];
+// save random color that needs guessed
+var randColor;
+// menubar items
+var btnNew document.getElementById("new");
+var btnEasy document.getElementById("easy");
+var btnHard document.getElementById("hard");
+var message document.getElementById("mid");
 
-buttonOne.addEventListener("click", function() {
-    if (oneH1.textContent < playTo && twoH1.textContent < playTo) {
-        oneH1.textContent = Number(oneH1.textContent) + 1;
-        if (oneH1.textContent == playTo) {
-            // works but less good
-            //oneH1.style.color = "green";
-            // not working ?
-            oneH1.classList.add("green");
-        }
-    }
-});
-
-buttonTwo.addEventListener("click", function() {
-    if (oneH1.textContent < playTo && twoH1.textContent < playTo) {
-        twoH1.textContent = Number(twoH1.textContent) + 1;
-        if (twoH1.textContent == playTo) {
-            twoH1.style.color = "green";
-        }
-    }
-});
-
-buttonReset.addEventListener("click", function() {
-    reset();
-});
-
-function reset() {
-    oneH1.textContent = "0";
-    // not working ?
-    oneH1.classList.remove("green");
-    //oneH1.style.color = "black";
-    twoH1.textContent = "0";
-    //twoH1.classList.toggle("green");
-    twoH1.style.color = "black";
+// generate a random int between min and max
+function randInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    // max and min inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// populate colors array with random rgbs
+function mkColors() {
+    var r, g, b;
+    for (i=0; i < boardSize; i++) {
+        r = randInt(0, 255);
+        g = randInt(0, 255);
+        b = randInt(0, 255);
+        colors[i] = String("rgb(" + r + ", " + g + ", " + b + ")");
+    }
+}
+
+// TODO
+function checkGuess() {
+    console.log("hello" + this.style.backgroundColor);
+}
+
+// all setup required to start a new game
+function startGame() {
+    mkColors();
+    randColor = colors[randInt(0, colors.length - 1)];
+    document.getElementById("randColor").textContent = randColor;
+
+    var pieces = document.querySelectorAll(".piece");
+    for (i=0; i < pieces.length; i++) {
+        pieces[i].style.backgroundColor = colors[i];
+        pieces[i].addEventListener("click", checkGuess);
+    }
+}
+
+// menubar functions
+
+startGame();
