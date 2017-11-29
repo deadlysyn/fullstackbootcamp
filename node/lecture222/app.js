10eaff11854117c5de26e6757a00b44f7eda53a6
“Mike <unixdoes@gmail.com>
2017-11-28 20:38:29 -0500

    initial import

---
+++ node/lecture222/app.js
@@ -0,0 +1,14 @@
+/*
+ * playing with npm
+ */
+
+var cm = require("cat-me");
+var knock = require("knock-knock-jokes");
+
+// random cat
+console.log(cm());
+// specific cat
+console.log(cm("nyan"));
+
+// knock knock joke
+console.log(knock());
