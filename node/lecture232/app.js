05eb1d2925a24a196919b51c82c88e49dfcc782f
Mike Hoskins <unixdoes@gmail.com>
2017-11-29 23:37:35 -0500

    add love route

--- node/lecture232/app.js
+++ node/lecture232/app.js
@@ -9,6 +9,11 @@ app.get("/", function(req, res) {
     res.render("home.ejs");
 });
 
+app.get("/love/:thing", function(req, res) {
+    var thing = req.params.thing;
+    res.render("love.ejs", {thing: thing});
+});
+
 app.listen(8080, "127.0.0.1", function() {
     console.log("Server started...");
 });
