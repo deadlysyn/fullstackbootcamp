09da1fccc2f59833e1e48b5034c936e9fcc9a2a5
Mike Hoskins <unixdoes@gmail.com>
2017-12-03 18:07:01 -0500

    refactor routes; trick for getting listen port from environment

--- node/lecture242/app.js
+++ node/lecture242/app.js
@@ -1,33 +1,38 @@
 /*
  * API exercise: talk to omdbapi.com
+ *
  */
 
 var express = require('express'),
+    app = express(),
+    port = parseInt(process.env.PORT, 10) || 8080,
     request = require('request'),
     bp = require('body-parser');
-
-var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bp.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-    request('http://omdbapi.com/?apikey=thewdb&s=star', function (err, resp, body) {
-    if (err) {
-        console.log("Something went wrong:");
-        console.log(err);
-    } else if (resp.statusCode == 200) {
-        res.render('home', {results: JSON.parse(body).Search});
-    }
-});
-
+    res.render('home');
 });
 
 // http://omdbapi.com/?apikey=thewdb&s=<name>
 // http://omdbapi.com/?apikey=thewdb&i=<id>
-app.post('/findmovie', function(req, res) {
+app.post('/search', function(req, res) {
+    var search = req.body.search;
+    var url = 'http://omdbapi.com/?apikey=thewdb&s=' + search;
+
+    request(url, function (err, resp, body) {
+        if (err) {
+            console.log("ERROR: something went wrong, dumping payload...");
+            console.log(err);
+        } else if (resp.statusCode == 200) {
+            res.render('results', {results: JSON.parse(body).Search});
+        }
+    });
+
 });
 
-app.listen(8080, '127.0.0.1', function(req, res) {
+app.listen(port, '127.0.0.1', function(req, res) {
     console.log('Server started...');
 });
