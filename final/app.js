c300799e26981d76e81a94f920586c125d15de37
Mike Hoskins <unixdoes@gmail.com>
2017-12-03 22:41:30 -0500

    new routes for adding campgrounds

--- final/app.js
+++ final/app.js
@@ -12,18 +12,30 @@ var express = require('express'),
 app.set('view engine', 'ejs');
 app.use(bp.urlencoded({extended: true}));
 
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+    {name: "Granite Hill", image: "https://farm8.staticflickr.com/7527/15944090146_cc72295efc.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1268/4671261338_a0e9f5e219.jpg"}
+];
+
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-        {name: "Granite Hill", image: "https://farm8.staticflickr.com/7527/15944090146_cc72295efc.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1268/4671261338_a0e9f5e219.jpg"}
-    ];
-
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res) {
+    campgrounds.push({
+        name: req.body.name,
+        image: req.body.image
+    });
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new');
 });
 
 app.listen(port, '127.0.0.1', function() {
