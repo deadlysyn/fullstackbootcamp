1595fdb14c64acb7edf79424cb3e158b0573ca66
Mike Hoskins <unixdoes@gmail.com>
2017-12-05 01:27:28 -0500

    refactor to use mongodb

--- final/app.js
+++ final/app.js
@@ -3,35 +3,50 @@
  *
  */
 
-var express = require('express'),
-    app = express(),
-    port = parseInt(process.env.PORT, 10) || 8080,
-    request = require('request'),
-    bp = require('body-parser');
+var express     = require('express'),
+    app         = express(),
+    port        = parseInt(process.env.PORT, 10) || 8080,
+    request     = require('request'),
+    bp          = require('body-parser'),
+    mongoose    = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.use(bp.urlencoded({extended: true}));
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name: "Granite Hill", image: "https://farm8.staticflickr.com/7527/15944090146_cc72295efc.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1268/4671261338_a0e9f5e219.jpg"}
-];
+mongoose.connect('mongodb://localhost/yelp_camp', {useMongoClient: true});
+// schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+// model
+var Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, function(err, campgrounds){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req, res) {
-    campgrounds.push({
+    Campground.create({
         name: req.body.name,
         image: req.body.image
+    }, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
     });
-    res.redirect('/campgrounds');
 });
 
 app.get('/campgrounds/new', function(req, res) {
