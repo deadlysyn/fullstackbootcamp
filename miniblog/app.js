a180a4805d838f20324f0b51ecf23bfd011193da
Mike Hoskins <unixdoes@gmail.com>
2017-12-05 20:06:05 -0500

    add root and blogs routes

--- miniblog/app.js
+++ miniblog/app.js
@@ -17,6 +17,20 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model('Blog', blogSchema);
 
+app.get('/', function(req, res) {
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {blogs: blogs});
+        }
+    });
+});
+
 app.listen(port, '127.0.0.1', function() {
     console.log('Server listening...');
 });
