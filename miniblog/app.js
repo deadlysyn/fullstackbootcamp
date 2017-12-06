5e4a36836d15be14def316bd0aba09b899cdd917
Mike Hoskins <unixdoes@gmail.com>
2017-12-05 23:09:09 -0500

    add new and create routes

--- miniblog/app.js
+++ miniblog/app.js
@@ -1,3 +1,7 @@
+/*
+ * Exercise MongoDB by creating a simple blog.
+ */
+
 var express     = require('express'),
     app         = express(),
     port        = parseInt(process.env.PORT, 10) || 8080,
@@ -27,6 +31,20 @@ app.get('/blogs', function(req, res) {
             console.log(err);
         } else {
             res.render('index', {blogs: blogs});
+        }
+    });
+});
+
+app.get('/blogs/new', function(req, res) {
+    res.render('new');
+});
+
+app.post('/blogs', function(req, res) {
+    Blog.create(req.body.blog, function(err, blog) {
+        if (err) {
+            res.redirect('/blogs/new');
+        } else {
+            res.redirect('/blogs');
         }
     });
 });
