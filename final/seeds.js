afe494d63f1a28bc5db092b410fcd405f07feed9
Mike Hoskins <unixdoes@gmail.com>
2017-12-08 17:06:26 -0500

    add lorem ipsum campground descriptions

--- final/seeds.js
+++ final/seeds.js
@@ -10,17 +10,17 @@ var data = [
     {
         name: "Frozen Field",
         image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg",
-        description: "Brrrr it's so coooold!"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate in felis quis porta. Nam vestibulum ex ornare, molestie lectus vel, pellentesque erat. Mauris auctor auctor neque non ultricies. Proin non mi tempor lectus sagittis egestas quis ac mauris. Sed ultricies, lacus ac egestas vestibulum, justo urna tempor elit, at sollicitudin mauris arcu vel tellus. Vivamus pulvinar risus ac pulvinar mattis. Maecenas gravida volutpat odio quis porttitor. Nullam quis augue eleifend, porta purus a, imperdiet mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse rutrum ipsum in lectus dapibus porta. In sit amet purus sit amet felis sollicitudin posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc gravida luctus enim, vitae tempor ante hendrerit vitae. Integer hendrerit erat blandit felis sollicitudin elementum. Cras congue sagittis ante. "
     },
     {
         name: "Treeless Bluff",
         image: "https://farm9.staticflickr.com/8302/7820598746_4d8c11899e.jpg",
-        description: "Lots of rocks, no bathrooms."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate in felis quis porta. Nam vestibulum ex ornare, molestie lectus vel, pellentesque erat. Mauris auctor auctor neque non ultricies. Proin non mi tempor lectus sagittis egestas quis ac mauris. Sed ultricies, lacus ac egestas vestibulum, justo urna tempor elit, at sollicitudin mauris arcu vel tellus. Vivamus pulvinar risus ac pulvinar mattis. Maecenas gravida volutpat odio quis porttitor. Nullam quis augue eleifend, porta purus a, imperdiet mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse rutrum ipsum in lectus dapibus porta. In sit amet purus sit amet felis sollicitudin posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc gravida luctus enim, vitae tempor ante hendrerit vitae. Integer hendrerit erat blandit felis sollicitudin elementum. Cras congue sagittis ante. "
     },
     {
         name: "Green Forest",
         image: "https://farm4.staticflickr.com/3164/2645148513_1a1824659c.jpg",
-        description: "Watch for poison oak, and ivy, and snakes."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate in felis quis porta. Nam vestibulum ex ornare, molestie lectus vel, pellentesque erat. Mauris auctor auctor neque non ultricies. Proin non mi tempor lectus sagittis egestas quis ac mauris. Sed ultricies, lacus ac egestas vestibulum, justo urna tempor elit, at sollicitudin mauris arcu vel tellus. Vivamus pulvinar risus ac pulvinar mattis. Maecenas gravida volutpat odio quis porttitor. Nullam quis augue eleifend, porta purus a, imperdiet mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse rutrum ipsum in lectus dapibus porta. In sit amet purus sit amet felis sollicitudin posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc gravida luctus enim, vitae tempor ante hendrerit vitae. Integer hendrerit erat blandit felis sollicitudin elementum. Cras congue sagittis ante. "
     }
 ];
 
