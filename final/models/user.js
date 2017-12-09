12edce48367a7721e61592302beac3c363c48062
Mike Hoskins <unixdoes@gmail.com>
2017-12-09 12:22:40 -0500

    add user model

---
+++ final/models/user.js
@@ -0,0 +1,15 @@
+/*
+ * User model
+ */
+
+var mongoose                = require('mongoose'),
+    passportLocalMongoose   = require('passport-local-mongoose');
+
+var UserSchema = new mongoose.Schema({
+    name: String,
+    password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
