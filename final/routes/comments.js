560f618571fd686b3e07f548dc05f3a936d26ac9
Mike Hoskins <unixdoes@gmail.com>
2017-12-13 16:57:19 -0500

    add middleware to check comment ownership

--- final/routes/comments.js
+++ final/routes/comments.js
@@ -41,7 +41,7 @@ router.post('/', isLoggedIn, function(req, res) {
 });
 
 // show form to edit comment
-router.get('/:comment_id/edit', function(req, res) {
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, comment) {
         if (err) {
             res.redirect('back');
@@ -52,7 +52,7 @@ router.get('/:comment_id/edit', function(req, res) {
 });
 
 // update comment
-router.put('/:comment_id', function(req, res) {
+router.put('/:comment_id', checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
         if (err) {
             res.redirect('back');
@@ -63,7 +63,7 @@ router.put('/:comment_id', function(req, res) {
 });
 
 // remove comment
-router.delete('/:comment_id', function(req, res) {
+router.delete('/:comment_id', checkCommentOwnership, function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if (err) {
             res.redirect('back');
@@ -79,6 +79,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect('/login');
+}
+
+// middleware to check comment ownership
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, comment) {
+            if (err) {
+                res.redirect('back');
+            } else {
+                if (comment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
 }
 
 module.exports = router;
