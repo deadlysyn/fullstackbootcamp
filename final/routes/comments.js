65a747217fbf112471faf6408852896e99b97045
Mike Hoskins <unixdoes@gmail.com>
2017-12-13 00:48:15 -0500

    add edit and update comment routes

--- final/routes/comments.js
+++ final/routes/comments.js
@@ -40,6 +40,28 @@ router.post('/', isLoggedIn, function(req, res) {
     });
 });
 
+// show form to edit comment
+router.get('/:comment_id/edit', function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.render('comments/edit', {campground_id: req.params.id, comment: comment});
+        }
+    });
+});
+
+// update comment
+router.put('/:comment_id', function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
 // middleware to check if user has logged in
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
